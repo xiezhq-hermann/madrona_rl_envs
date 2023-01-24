a1f690b9f632b7f344a9f535a662a63ee6cc5e6d
Zhiqiang Xie <xiezhq@stanford.edu>
2023-01-23 21:22:38 -0800

    update the timing for hanabi example

--- scripts/hanabi_example.py
+++ scripts/hanabi_example.py
@@ -47,7 +47,16 @@ else:
 old_state = env.n_reset()
 actions = torch.zeros((2, args.num_envs, 1), dtype=int).to(device=env.device)
 num_errors = 0
-start_time = time.time()
+
+# warp up
+for i in range(5):
+    for i in range(2):
+        logits = torch.rand(args.num_envs, env.action_space.n).to(device=env.device)
+        logits[torch.logical_not(old_state[i].action_mask)] = -float('inf')
+        actions[i, :, 0] = torch.max(logits, dim=1).indices  # torch.randint_like(actions, high=4)
+    next_state, reward, next_done, _ = env.n_step(actions)
+
+time_stamps = [0 for i in range(args.num_steps * 2)]
 for iter in tqdm(range(args.num_steps), desc="Running Simulation"):
     for i in range(2):
         logits = torch.rand(args.num_envs, env.action_space.n).to(device=env.device)
@@ -55,7 +64,9 @@ for iter in tqdm(range(args.num_steps), desc="Running Simulation"):
         actions[i, :, 0] = torch.max(logits, dim=1).indices  # torch.randint_like(actions, high=4)
     # print(actions)
 
+    time_stamps[iter * 2] = time.time()
     next_state, reward, next_done, _ = env.n_step(actions)
+    time_stamps[iter * 2 + 1] = time.time()
 
     if args.validation and not validate_step(old_state, actions, next_done, next_state, reward, han_conf, verbose=args.verbose):
         num_errors += 1
@@ -63,6 +74,8 @@ for iter in tqdm(range(args.num_steps), desc="Running Simulation"):
 
     old_state = next_state
     
-print("SPS:", args.num_steps / (time.time() - start_time))
+time_difference = [time_stamps[i] - time_stamps[i-1] for i in range(1, len(time_stamps), 2)]
+assert(len(time_difference) == args.num_steps)
+print("step * worlds / sec:", args.num_envs / (sum(time_difference) / args.num_steps))
 if args.validation:
     print("Error rate:", num_errors/args.num_steps)
